618dad691637f36d90432bec3b29d1fb666f1fc0
BeroMaze <Berning.corey@gmail.com>
2016-01-10 16:19:07 -0800

    add object function

--- script.js
+++ script.js
@@ -18,3 +18,25 @@ $("#proImg").on('click', function(event) {
 $("#homeImg").on('click', function(event) {
   window.open('./index.html', '_self');
 });
+var count = 1;
+var index = 0;
+var allProjects = [];
+
+var Projects = function(title, img, link, miniDesc, description){
+  this.title = title;
+  this.img = img;
+  this.link = link;
+  this.miniDesc = miniDesc;
+  this.description = description;
+  allProjects.push(this);
+};
+
+projects.map(function(count, index){
+  count = new Projects(projects[index].title, projects[index].img, projects[index].link, projects[index].description);
+  count += 1;
+  index += 1;
+});
+
+Pojects.prototype.html = function () {
+  $('#iconView').append('<article class="articles"><img class="projectIcon"src=' + this.img + '/><h3 class="iconTilte">' + this.title + '</h3><p class="descIcon">' + this.miniDesc + '</p><a href=' + this.link + 'class="descIcon">See Finished site.</a></article>');
+};
