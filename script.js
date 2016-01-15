0e4e087a6161e5b878ab787bb94b7e2f1bb27234
BeroMaze <Berning.corey@gmail.com>
2016-01-15 14:17:48 -0800

    add handlebars and filter. set up to rerender main html

--- script.js
+++ script.js
@@ -45,14 +45,49 @@ $('#proImg').on('click', function(event) {
       'background-image': 'url("./img/iconPageBackground.jpg")',
       'background-repeat': 'no-repeat',
       'background-size': 'cover'});
-    $('#mainView').prepend('<h2 id="projectTitle">Programing Project</h2><p id="projectDesc">Take a look trough all the different projects that I have done since I have Started Programing.</p>');
+    $('#mainView').prepend('<h2 id="projectTitle">Programing Project</h2><select id="catagoryFilter"><option value="catagory">catagory</option></select><p id="projectDesc">Take a look trough all the different projects that I have done since I have Started Programing.</p>');
     iconsToPage();
     pickArticles();
+    var newArrayC = projects.map(function(p){
+      return '<option value="' + p.catagory + '">' + p.catagory + '</option>';
+    });
+    var uniqueArray = [];
+    $.each(newArrayC, function(i, el){
+      if($.inArray(el, uniqueArray) === -1) uniqueArray.push(el);
+    });
+    $(document).ready(function() {
+      uniqueArray.forEach(function(catagory){
+        $('#catagoryFilter').append(catagory);
+      });
+
+      $('#catagoryFilter').on('change', function() {
+        if($(this).val() === 'catagory') {
+          $('.articles').show();
+        }
+        else if ($(this).val()) {
+          console.log($(this).val());
+          $('.articles').hide();
+          $('.articles[value*=' + $(this).val() + ']').show();
+        }
+      });
+    });
   }, 200);
+
   setTimeout(function(){
     $('.navImg').not('#'+$(this).attr('id')).css('opacity', '1');
   }, 300);
 });
+
+
+
+var renderHome = function(){
+  $('#mainView').html('<script id="mainScript" type="text/x-handlebars-template"><div id="mainTop"><h2 id="mainTitle">{{title}}</h2></div><div id="mainMiddle"><div class="middleLeft"><div class="overFlow"><p class="mainWords">{{desc1}}</p></div><div class="overFlow"><p class="mainWords">{{desc2}}</p></div></div><div class="middleCenter"><img src="./img/corey.jpg" id="coreyImg" /><h4 id="plac">{{name}}</h4></div><div class="middleRight"><div class="overFlow"><p class="mainWords">{{desc3}}</p></div><div class="overFlow"><p class="mainWords">{{desc4}}</p></div></div></div><div id="mainBottom"><p class="mainWords">{{desc5}}</p></div></div></script>');
+  var template = $('#mainScript').html();
+  var compiledTemplate = Handlebars.compile(template);
+  var html = compiledTemplate(homePage);
+  $('#mainView').append(html);
+};
+renderHome();
 
 $('#homeImg').on('click', function(event) {
   $('.navImg').not('#'+$(this).attr('id')).css('opacity', '.3');
@@ -61,23 +96,21 @@ $('#homeImg').on('click', function(event) {
       'background-image': 'none',
       'background-color': 'rgb(22, 1, 64)',
     });
-    $('#mainView').html('<script id="mainScript" type="text/x-handlebars-template"><div id="mainTop"><h2 id="mainTitle">{{title}}</h2></div><div id="mainMiddle"><div class="middleLeft"><div class="overFlow"><p class="mainWords">{{desc1}}</p></div><div class="overFlow"><p class="mainWords">{{desc2}}</p></div></div><div class="middleCenter"><img src="./img/corey.jpg" id="coreyImg" /><h4 id="plac">{{name}}</h4></div><div class="middleRight"><div class="overFlow"><p class="mainWords">{{desc3}}</p></div><div class="overFlow"><p class="mainWords">{{desc4}}</p></div></div></div><div id="mainBottom"><p class="mainWords">{{desc5}}</p></div></div></script>');
-    var template = $('#mainScript').html();
-    var compiledTemplate = Handlebars.compile(template);
-    var html = compiledTemplate(homePage);
-    $('#mainView').append(html);
+    renderHome();
   }, 200);
   setTimeout(function(){
     $('.navImg').not('#'+$(this).attr('id')).css('opacity', '1');
   }, 300);
 });
 
+
 var count = 1;
 var index = 0;
 var allProjects = [];
 
-var Projects = function(id, title, img, link, publishedOn, miniDesc, description){
+var Projects = function(id, catagory, title, img, link, publishedOn, miniDesc, description){
   this.id = id;
+  this.catagory = catagory;
   this.title = title;
   this.img = img;
   this.link = link;
@@ -88,14 +121,14 @@ var Projects = function(id, title, img, link, publishedOn, miniDesc, description
 };
 
 projects.map(function(count, index){
-  count = new Projects(projects[index].id, projects[index].title, projects[index].img, projects[index].link, projects[index].publishedOn, projects[index].miniDesc);
+  count = new Projects(projects[index].id, projects[index].catagory, projects[index].title, projects[index].img, projects[index].link, projects[index].publishedOn, projects[index].miniDesc);
   count += 1;
   index += 1;
 });
 
 Projects.prototype.html = function () {
   var date = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-  $('#iconView').append('<article id=' + this.id + ' class="articles"><img class="projectIcon"src=' + this.img + '/><h3 class="iconTilte">' + this.title + '</h3><p class="publishedOn">Created: ' + date + ' days ago</p><p class="descIcon">' + this.miniDesc + '</p><a href=' + this.link + 'class="descIcon">See Finished site.</a></article>');
+  $('#iconView').append('<article id=' + this.id + ' class="articles" value="' + this.catagory + '"><img class="projectIcon"src=' + this.img + '/><h3 class="iconTilte">' + this.title + '</h3><p class="publishedOn">Created: ' + date + ' days ago</p><p class="descIcon">' + this.miniDesc + '</p><a href=' + this.link + 'class="descIcon">See Finished site.</a></article>');
 };
 
 var iconsToPage = function(){
@@ -109,7 +142,7 @@ var pickArticles = function(){
     $pick = this.id;
     $('.articles').not('#'+$pick).css('opacity', '.3');
     setTimeout(function(){
-      $('#mainView').html('<script id="hello" type="text/template"><h1 class="FullViewTitle">{{title}}</h1><a href={{link}} class="fullViewLink">Check Out The Site! </a><img src= {{img}} class="fullViewImg"/><p3 class="fullViewDesc"> {{description}} </p3><p4 class="created">Created: {{publishedOn}} days ago</p4></script>');
+      $('#mainView').html('<script id="hello" type="text/template"><h1 class="FullViewTitle">{{title}}</h1><a href={{link}} class="fullViewLink">Check Out The Site! </a><img src= {{img}} class="fullViewImg"/><p3 class="fullViewDesc"> {{description}} </p3><p4 class="created">Created on: {{publishedOn}} </p4></script>');
       $('main').css({
         'background-image': 'none',
         'background-color': 'rgb(54, 14, 64)',
